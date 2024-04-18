32cbe4d67227f6a658bb955a0f3664ed00c93fd7
Madusalves <malvesduarte444@gmail.com>
2024-04-17 21:34:59 -0300

    build models and store and manage items

--- Hoch/item/models.py
+++ Hoch/item/models.py
@@ -1,7 +1,29 @@
 from django.db import models
 from django.db import migrations
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=255) 
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    # Name the categories in admin
+    def __str__(self):
+        return self.name
+    
+
+    # store and manage items
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name="items", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_image', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    # who created and what time was created
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
